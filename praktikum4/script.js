88e732c9d5808c2b1b3e5a060019d18db82bc891
aufall02 <aufaluzumaki@gmail.com>
2022-07-13 15:01:36 +0700

    praktikumday4

---
+++ praktikum4/script.js
@@ -0,0 +1,31 @@
+// var a = 10;
+// var b = 4;
+// var c = a + b;
+// var input = document.getElementById("txinp")
+// var angka1 = document.getElementById("angka1")
+// var angka2 = document.getElementById("angka2")
+
+console.log('oiiii');
+document.getElementById('coba').
+innerHTML = "program javascript pertama saya."
+document.getElementById('coba1').
+innerHTML = "ini bariskedua"
+
+// document.write("hasil a + b = "+c)
+
+function getInput(){
+    alert("hallo "+input.value)
+}
+function getLuasPersegi(){
+    var angka1 = document.getElementById("angka1")
+    var angka2 = document.getElementById("angka2")
+    var hasilLuasPersegi = angka1.value * angka2.value;
+    document.getElementById('hasil').innerHTML = +hasilLuasPersegi ;
+}
+function getLuasSegiTiga(){
+    var hasilSegiTiga = angka1.value * angka2.value / 2 ;
+    document.getElementById('hasil').innerHTML = hasilSegiTiga;
+}
+function getRiset(){
+    
+}
